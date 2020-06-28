040bb4f164e9e3fa64d6fd32234dfb68ac711302
Michael R <mruhaak@gmail.com>
2020-06-28 08:05:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
-require 'prime'
+range = [1..100]
 
-Prime.each(27) do |prime|
-  p prime
+def prime(number)
+  if range.each.prime?
+    return true
+  elseif number <= 0
+  return "Be sure to account for negative numbers!"
+else
+  return false
+end
 end
